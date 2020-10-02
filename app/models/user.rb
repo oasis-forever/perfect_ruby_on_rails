fc202a71ff46591f906235207fbf289f2e7107a8
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-10-02 15:44:11 +0900

    Implement oauth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  class << self
+    def find_or_create_from_auth_hash!(auth_hash)
+      provider  = auth_hash[:provider]
+      uid       = auth_hash[:uid]
+      nickname  = auth_hash[:info][:nickname]
+      image_url = auth_hash[:info][:image]
+
+      self.find_or_create_by!(provider: provider, uid: uid) do |user|
+        user.name      = nickname
+        user.image_url = image_url
+      end
+    end
+  end
 end
