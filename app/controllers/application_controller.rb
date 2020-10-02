101a21703b615cc7084603537b298633bb99fd81
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-10-02 16:30:06 +0900

    Implement oauth logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :logged_in?
+
+  private
+
+  def logged_in?
+    # Return false in case of false and nil
+    # Otherwise, return true
+    !!session[:user_id]
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,9 @@ class SessionsController < ApplicationController
     session['user_id'] = user.id
     redirect_to root_path, notice: 'You\'ve successfully logged in!'
   end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'You\'ve successfully logged out!'
+  end
 end
