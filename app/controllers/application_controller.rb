e27e98bf6eba990180b16061a51b6ae08dad7069
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-10-06 11:20:03 +0900

    Fix current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def authenticate
