42351a636d5d8c4134e405486c31c92ed8055b43
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-11-16 16:25:57 +0900

    FIXME: Commit Event#new

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -6,4 +6,10 @@ class EventsTest < ApplicationSystemTestCase
     visit event_path(event)
     assert_selector 'h1', text: event.name
   end
+
+  test 'Visit Event New' do
+    sign_in_as(FactoryBot.create(:user))
+    visit new_event_path
+    assert_selector 'h1', text: 'イベント作成'
+  end
 end
