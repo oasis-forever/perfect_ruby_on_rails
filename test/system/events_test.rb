49042cf57f8f606539e8f6cbbe303e73f02aaee3
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-11-16 23:20:16 +0900

    Fix selector in Event#new

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -10,6 +10,6 @@ class EventsTest < ApplicationSystemTestCase
   test 'Visit Event New' do
     sign_in_as(FactoryBot.create(:user))
     visit new_event_path
-    assert_selector 'h1', text: 'イベント作成'
+    assert_selector 'h1', text: '新規イベント登録'
   end
 end
