ea6911e6249187ce62a30a0374dfe7d4834b6599
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-11-19 22:28:18 +0900

    Make available SignInHelper in all tests

--- test/sign_in_helper.rb
+++ test/sign_in_helper.rb
@@ -9,12 +9,23 @@ module SignInHelper
           image: user.image_url
         }
     )
-    visit root_path
-    click_on 'GitHubでログイン'
+    case
+    when respond_to?(:visit)
+      visit root_path
+      click_on 'GitHubでログイン'
+    when respond_to?(:get)
+      get '/auth/github/callback'
+    else
+      raise NotImplementedError.new
+    end
     @current_user = user
   end
 
   def current_user
     @current_user
+  end
+
+  class ActionDispatch::IntegrationTest
+    include SignInHelper
   end
 end
