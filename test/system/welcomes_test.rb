f1d7cd1032f9540bb7d6ba913b3a492b9bd47af0
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-11-19 22:00:28 +0900

    Update WelcomesTest

--- test/system/welcomes_test.rb
+++ test/system/welcomes_test.rb
@@ -5,4 +5,15 @@ class WelcomesTest < ApplicationSystemTestCase
     visit root_path
     assert_selector 'h1', text: 'イベント一覧'
   end
+
+  test 'Past Events are hidden' do
+    future_event = FactoryBot.create(:event, start_at: Time.zone.now.since(3.days))
+    past_event   = FactoryBot.create(:event, start_at: Time.zone.now.since(1.day))
+
+    travel_to(Time.zone.now.since(2.days)) do
+      visit root_path
+      assert_selector 'h5', text: future_event.name
+      assert_no_selector 'h5', text: past_event.name
+    end
+  end
 end
