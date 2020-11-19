f37f8114e31803cd39767f37c77a7431815b6873
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-11-19 22:45:56 +0900

    Commit EventTest and fix Event#created_by?

---
+++ test/models/event_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class EventTest < ActiveSupport::TestCase
+  test "Argument ID of #created_by? is equal to owner_id" do
+    event = FactoryBot.create(:event)
+    assert_equal(true, event.created_by?(event.owner))
+  end
+
+  test "Argument ID of #created_by? is not equal to owner_id" do
+    event = FactoryBot.create(:event)
+    user  = FactoryBot.create(:user)
+    assert_equal(false, event.created_by?(user))
+  end
+
+  test "Argument ID of #created_by? is equal to nil" do
+    event = FactoryBot.create(:event)
+    assert_equal(false, event.created_by?(nil))
+  end
+end
