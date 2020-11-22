ca4e13a4055d4068898ac7a24c3e36f76c5024ff
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-11-22 15:02:13 +0900

    Apply image validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,6 +13,10 @@ class Event < ApplicationRecord
   validates :start_at, presence: true
   validates :end_at, presence: true
   validate :start_at_should_be_before_end_at
+  validates :image,
+    content_type: %i(png jpg jpeg),
+    size: { less_than_or_equal_to: 10.megabytes },
+    dimension: { width: { max: 2000 }, height: { max: 2000 } }
 
   def created_by?(user)
     return false unless user
