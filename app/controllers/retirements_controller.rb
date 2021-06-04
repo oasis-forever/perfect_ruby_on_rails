1ad5211813cbf49f66f452fcd4a191e0e009781a
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-06-04 12:29:27 +0900

    Refactor RetirementsController#create

--- app/controllers/retirements_controller.rb
+++ app/controllers/retirements_controller.rb
@@ -3,9 +3,8 @@ class RetirementsController < ApplicationController
   end
 
   def create
-    if current_user.destroy
-      reset_session
-      redirect_to root_path, notice: '退会が完了しました'
-    end
+    return unless current_user.destroy
+    reset_session
+    redirect_to root_path, notice: '退会が完了しました'
   end
 end
