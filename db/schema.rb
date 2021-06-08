27b484ec127199a701ffbc6986bfe84b241cc13a
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-06-08 13:58:20 +0900

    Update ActiveStorage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_031153) do
+ActiveRecord::Schema.define(version: 2021_06_08_045127) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.string "name", null: false
@@ -30,7 +30,14 @@ ActiveRecord::Schema.define(version: 2020_11_22_031153) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "events", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
@@ -66,5 +73,6 @@ ActiveRecord::Schema.define(version: 2020_11_22_031153) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "tickets", "events"
 end
